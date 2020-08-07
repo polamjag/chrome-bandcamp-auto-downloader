4053f6c6f3eaeb6f35f4137a6efe3a3d2ac00af9
polamjag <s@polamjag.info>
2020-08-07 23:07:40 +0900

    appned message after button click

--- src/js/content.js
+++ src/js/content.js
@@ -1,12 +1,21 @@
 (() => {
+  const downloadBtn = document.createElement("button");
+
   const fireDownload = () => {
     const urls = Array.from(
       document.querySelectorAll(".download-title a")
     ).map((el) => el.getAttribute("href"));
     chrome.runtime.sendMessage({ command: "exec_download_urls", urls });
+
+    downloadBtn.innerText = "Downloading All Purchases..."
+
+    const msg = document.createElement('div');
+    msg.innerText = 'Not working well? Please reload page and retry';
+    msg.style.padding = '.8em';
+    msg.style.backgroundImage = '#1111';
+    downloadBtn.insertAdjacentElement('afterend', msg);
   };
 
-  const downloadBtn = document.createElement("button");
   downloadBtn.innerText = "Auto Download All Purchases";
   downloadBtn.style.margin = "0 0 0 8px";
   downloadBtn.style.padding = ".2em .3em";
